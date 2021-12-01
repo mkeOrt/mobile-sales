21a797fed7f2365441006c8b76b7a3f6108317f0
Mike <mke.ort@gmail.com>
2021-12-01 12:03:15 -0600

    Scanning barcodes in main screen

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, Fragment } from 'react';
+import { Text, View } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+import { Camera } from 'expo-camera';
+import { FAB, Icon } from 'react-native-elements';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [isUsingCamera, setIsUsingCamera] = useState(false);
+  const [code, setCode] = useState(null);
+
+  const onBarCodeScanned = ({ data }) => {
+    setIsUsingCamera(false);
+    setCode(data)
+  }
+
+  const openCamera = () => {
+    setIsUsingCamera(true);
+  }
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={tw`h-full`, !isUsingCamera ? [{flex: 1, justifyContent: 'center', alignItems: 'center'}]: ''}>
+      {isUsingCamera &&
+        <View style={tw`h-1/2 m-5 mt-10`}>
+          <View style={tw`h-full`}>
+            <Camera type="back" style={tw`h-full w-full`} onBarCodeScanned={onBarCodeScanned} />
+          </View>
+        </View>
+      }
+      {!isUsingCamera && <Text>{code ? code : 'Código no escaneado'}</Text>}
+      {!isUsingCamera &&
+        <FAB
+          color="dodgerblue"
+          icon={<Icon type="antdesign" name="camera" color="white" />}
+          placement="right"
+          onPress={openCamera}
+        />
+      }
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
