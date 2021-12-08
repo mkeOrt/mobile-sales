3c87c0a0ea9800531372f7b26fc2485fef1c35b1
Mike <mke.ort@gmail.com>
2021-12-08 14:07:30 -0600

    Adding dotenv configuration

--- pages/Home.js
+++ pages/Home.js
@@ -3,22 +3,25 @@ import { SafeAreaView, Text, View } from 'react-native';
 import { FAB, Icon } from 'react-native-elements';
 import ProductsList from '../components/ProductsList';
 import tw from 'tailwind-react-native-classnames';
+import { MY_NAME } from '@env'
 
 
 const Home = ({ navigation }) => {
   const [products, setProducts] = useState([]);
 
-  useEffect(async () => {
-    const data = await fetch('https://randomuser.me/api/?results=20')
-      .then((response) => response.json())
-    setProducts(data.results);
+  useEffect(() => {
+    (async () => {
+      const data = await fetch('https://randomuser.me/api/?results=20')
+        .then(res => res.json());
+      setProducts(data.results);
+    })();
   }, []);
 
   return (
     <SafeAreaView style={tw`h-full bg-white`}>
-      <View style={tw`mt-10 pl-3`}>
+      {/* <View style={tw`android:mt-10 pl-3`}>
         <Text style={tw`text-3xl font-medium`}>Productos</Text>
-      </View>
+      </View> */}
       <ProductsList products={products} />
       <FAB
         placement="right"
